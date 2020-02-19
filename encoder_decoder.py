583ca2f4429940a3b1fe4ed6c21da2b85cae3e66
HARSH SINGH <41187121+HARSHSINGH0@users.noreply.github.com>
2020-02-19 10:39:10 +0530

    program will now run on while loop

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -28,23 +28,27 @@ y->b
 z->a
 """
 try:
-	print("\nNote : Your message should be under a-z (lowercase alphabets) ,A-Z (uppercase alphabets) and Numbers")
 	def encoder_decoder():
-		dir={'a':'z','b':'y','c':'x','d':'w','e':'v','f':'u','g':'t','h':'s',
-		'i':'r','j':'q','k':'p','l':'o','m':'n','n':'m','o':'l','p':'k','q':'j',
-		'r':'i','s':'h','t':'g','u':'f','v':'e','w':'d','x':'c','y':'b','z':'a',' ':' '
-		,'A':'Z','B':'Y','C':'X','D':'W','E':'V','F':'U','G':'T','H':'S','I':'R','J':'Q',
-		'K':'P','L':'O','M':'N','N':'M','O':'L','P':'K','Q':'J','R':'I','S':'H','T':'G','U':'F',
-		'V':'E','W':'D','X':'C','Y':'B','Z':'A',1:0,2:9,3:8,4:7,5:6,6:5,7:4,8:3,9:2,0:1}
-		value=input("enter your message to encode or decode :")
-		value2=[]
-		for i in value:
-			value2.append(i)
+			dir={'a':'z','b':'y','c':'x','d':'w','e':'v','f':'u','g':'t','h':'s',
+			'i':'r','j':'q','k':'p','l':'o','m':'n','n':'m','o':'l','p':'k','q':'j',
+			'r':'i','s':'h','t':'g','u':'f','v':'e','w':'d','x':'c','y':'b','z':'a',' ':' '
+			,'A':'Z','B':'Y','C':'X','D':'W','E':'V','F':'U','G':'T','H':'S','I':'R','J':'Q',
+			'K':'P','L':'O','M':'N','N':'M','O':'L','P':'K','Q':'J','R':'I','S':'H','T':'G','U':'F',
+			'V':'E','W':'D','X':'C','Y':'B','Z':'A',1:0,2:9,3:8,4:7,5:6,6:5,7:4,8:3,9:2,0:1}
+			value=input("enter your message to encode or decode :")
+
+			value2=[]
+			for i in value:
+				value2.append(i)
+			print("Answer :",end="")
+			for i in value2:
+				printer=dir['{}'.format(i)]
+				print(printer,end="")
 		
-		for i in value2:
-			printer=dir['{}'.format(i)]
-			print(printer,end="")
-	encoder_decoder()
+	while True:
+		print("\nNote : Your message should be under a-z (lowercase alphabets) ,A-Z (uppercase alphabets) and Numbers \n CTRL+C to close program")
+		encoder_decoder()
+	input()
 	
 	
 except Exception as e:
@@ -54,3 +58,6 @@ except Exception as e:
 		encoder_decoder()
 	else:
 		print("bye")
+
+finally:
+	print("Program closed")
